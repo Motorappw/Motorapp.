40395d6577a51156c8768f8d51e4248fe30c1594
Motorappw <seguarumo@gmail.com>
2025-07-09 18:13:49 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'motorappsecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route("/")
+def welcome():
+    return render_template("welcome.html")
+
+@app.route("/home")
+@login_required
+def home():
+    return render_template("home.html", user=current_user)
+
+@app.route("/marca/<nombre>")
+@login_required
+def marca(nombre):
+    return render_template("marca.html", nombre=nombre.lower())
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for("home"))
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if not User.query.filter_by(username=username).first():
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for("login"))
+    return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("welcome"))
+
+if __name__ == "__main__":
+    if not os.path.exists("users.db"):
+        db.create_all()
+    app.run(debug=True)
